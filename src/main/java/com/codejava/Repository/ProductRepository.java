1fc321b76e12acc72278d8ae78e9090bf32c99b4
hieu <minhhieuvux@gmail.com>
2021-09-04 11:47:41 +0700

    update repository search

--- src/main/java/com/codejava/Repository/ProductRepository.java
+++ src/main/java/com/codejava/Repository/ProductRepository.java
@@ -2,6 +2,11 @@ package com.codejava.Repository;
 
 import com.codejava.Product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository <Product , Long> {
+    @Query("SELECT p from Product p where p.name LIKE %?1%")
+    public List<Product> findAll(String keyword);
 }
